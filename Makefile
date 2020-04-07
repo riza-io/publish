47155af17b958f5d03fbb805e5cdc68e45d19590
Andrew Benton <andrew@riza.io>
2020-04-07 15:09:10 -0700

    replace downloaded equinox riza client with node grpc implementation

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 dist/index.js: lib/main.js node_modules
 	./node_modules/.bin/ncc build lib/main.js -o dist
 
-lib/main.js: src/main.ts node_modules
+lib/main.js: src/main.ts node_modules tsconfig.json
 	./node_modules/.bin/tsc
 
 node_modules:
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
 import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import * as tc from '@actions/tool-cache';
+import { readFileSync } from 'fs';
+import grpc = require('grpc');
+
+import services = require('./riza/web_grpc_pb');
+import messages = require('./riza/web_pb');
+
+const apiClient = new services.APIClient('grpc-web.riza.io', grpc.credentials.createSsl());
 
 async function run() {
-  const assetId = '3soHzVjmQ48';
-  const cliPath = await tc.downloadTool(`https://bin.equinox.io/a/${assetId}/riza-linux-amd64.tar.gz`);
-  const extPath = await tc.extractTar(cliPath);
-  
-  const cachedPath = await tc.cacheDir(extPath, 'riza', assetId);
-  core.addPath(cachedPath);
-  
   try {
     const project = core.getInput('project');
-    const version = core.getInput('version');
     const path = core.getInput('path');
-    await exec.exec('riza', [project, version, path]);
+    const protoBytes = readFileSync(path);
+
+    const req = new messages.PublishDescriptorsReq();
+    req.setProjectId(project);
+    const protosMap = req.getProtosMap();
+    protosMap.set(path, protoBytes);
+
+    await function(): Promise<messages.PublishDescriptorsResp> {
+      return new Promise<messages.PublishDescriptorsResp>((resolve, reject) => {
+        apiClient.publishDescriptors(req, null, (err: grpc.callError, resp: messages.PublishDescriptorsResp) => {
+          err ? reject(err) : resolve(resp);
+        });
+      });
+    }();
   } catch (error) {
     core.setFailed(error.message);
   }
